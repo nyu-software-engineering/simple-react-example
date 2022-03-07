cacac94faf260be528b26b474f50f7a7961b303a
bloombar <amos@cs.nyu.edu>
2022-03-07 12:57:04 -0500

    added routes

---
+++ src/About.js
@@ -0,0 +1,5 @@
+const About = props => {
+  return <p>Hello world!</p>
+}
+
+export default About
---
+++ src/Home.js
@@ -0,0 +1,66 @@
+import { Link } from "react-router-dom"
+import { useEffect } from "react"
+import "./Home.css"
+import Puppy from "./Puppy"
+import PuppyForm from "./PuppyForm"
+
+const Home = () => {
+  useEffect(() => {
+    // this function runs once when the component first loads
+    // pop up an annoying message in the web browser
+    alert(
+      "hello world from within Home.js!\nPlease open your browser's Developer Tools -> Javascript Console"
+    )
+    // print it more discretely to the Developer Tools -> Javascript Console
+    console.log("hello world from within Home.js!")
+  }, []) // the empty array here is what causes this to run only once
+
+  // a function that will be run anytime a user clicks on a puppy article
+  const handleClick = e => {
+    alert("You clicked a puppy!")
+    console.log("You clicked a puppy!")
+  }
+
+  // an array of puppy data... imagine this is fetched from a back-end server API
+  // we hard-code it here so we can focus on React.js, not back-end code
+  const puppies = [
+    {
+      name: "Spot",
+      breed: "Schnauzer",
+    },
+    {
+      name: "Tobik",
+      breed: "German Shepherd",
+    },
+    {
+      name: "Fido",
+      breed: "Chihuahua",
+    },
+    {
+      name: "Kenny",
+      breed: "Beagle",
+    },
+  ]
+
+  return (
+    <main className="Home">
+      <h1>Hello to the app</h1>
+      <p>
+        Lorem <Link to="/about">ipsum dolor</Link> sit amet foo bar baz bum
+      </p>
+      <PuppyForm /> {/* show the form for adding a new puppy */}
+      <section class="Home-puppies">
+        {/* loop through the array of puppy data, and return a component for each object therein */}
+        {puppies.map((puppy, i, puppiesArray) => (
+          <Puppy
+            name={puppy.name}
+            breed={puppy.breed}
+            handleClick={handleClick}
+          />
+        ))}
+      </section>
+    </main>
+  )
+}
+
+export default Home
